e0478d1f63d262da2565bc9ff8331a14f1e4b06c
Oleg Nyunkov <nvo84@yandex.ru>
2022-10-19 23:47:31 +0600

    dev: stack wo animation

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./queue-page.module.css";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {Circle} from "../ui/circle/circle";
 
 export const QueuePage: React.FC = () => {
+
   return (
     <SolutionLayout title="Очередь">
-
+      <div className={styles.queue__controls}>
+        <div className={styles.queue__input}>
+          <Input
+            type="number"
+            isLimitText={true}
+            max={4}
+            />
+          <Button
+            text="Добавить"
+            />
+          <Button
+            text="Удалить"
+            />
+        </div>
+        <Button
+          text="Очистить"
+          />
+      </div>
+      <div className={styles.circle__container}>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+          <Circle/>
+      </div>
     </SolutionLayout>
   );
 };
--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,60 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, {useState} from "react";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import styles from "./stack-page.module.css";
+import {Circle} from "../ui/circle/circle";
 
 export const StackPage: React.FC = () => {
+  const [inputValue, setInputValue] = useState<number>()
+  const [stackArray, setStackArray] = useState<number[]>([])
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(+e.target.value)
+  }
+
+  const addToStack = () => {
+    inputValue && stackArray.push(inputValue)
+    setStackArray([...stackArray])
+  }
+
+  const removeFromStack = () => {
+    stackArray.pop()
+    setStackArray([...stackArray])
+  }
+
+  const clearStack = () => {
+    setStackArray([])
+  }
+
   return (
     <SolutionLayout title="Стек">
-
+      <div className={styles.stack__controls}>
+        <div className={styles.stack__input}>
+          <Input
+            type="number"
+            isLimitText={true}
+            max={4}
+            onChange={onChange}/>
+          <Button
+            text="Добавить"
+            onClick={addToStack}/>
+          <Button
+            text="Удалить"
+            onClick={removeFromStack}/>
+        </div>
+        <Button
+          text="Очистить"
+          onClick={clearStack}/>
+      </div>
+      <div className={styles.circle__container}>
+        {stackArray.map((char, index) => {
+          return <Circle
+            key={index}
+            letter={char}
+            index={index}
+          />
+        })}
+      </div>
     </SolutionLayout>
   );
 };
