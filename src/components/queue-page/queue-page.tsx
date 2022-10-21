4de4c75cd04f52eb907400908c9b564d2bb26438
Oleg Nyunkov <nvo84@yandex.ru>
2022-10-21 09:34:44 +0600

    dev: queue wo animation

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,12 +1,41 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, {useState} from "react";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
 import styles from "./queue-page.module.css";
 import {Input} from "../ui/input/input";
 import {Button} from "../ui/button/button";
 import {Circle} from "../ui/circle/circle";
 
 export const QueuePage: React.FC = () => {
+  const array = Array.from({length: 7}, () => null)
+  const [inputValue, setInputValue] = useState<number>()
+  const [stackQueue, setStackQueue] = useState<number[] | null[]>([...array])
+  const [addCount, setAddCount] = useState<number>(0)
+  const [removeCount, setRemoveCount] = useState<number>(0)
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(+e.target.value)
+  }
 
+  const addToQueue = () => {
+    inputValue && stackQueue.splice(addCount, 1, inputValue)
+    setStackQueue([...stackQueue])
+    const counter = addCount + 1
+    setAddCount(counter)
+  }
+
+  const removeFromQueue = () => {
+    delete(stackQueue[removeCount])
+    setStackQueue([...stackQueue])
+    const counter = removeCount + 1
+    setRemoveCount(counter)
+  }
+
+  const clearQueue = () => {
+    setStackQueue([...array])
+    setAddCount(0)
+    setRemoveCount(0)
+  }
+
+  console.log(addCount, removeCount)
   return (
     <SolutionLayout title="Очередь">
       <div className={styles.queue__controls}>
@@ -15,26 +44,29 @@ export const QueuePage: React.FC = () => {
             type="number"
             isLimitText={true}
             max={4}
-            />
+            onChange={onChange}
+          />
           <Button
             text="Добавить"
-            />
+            onClick={addToQueue}
+          />
           <Button
             text="Удалить"
-            />
+            onClick={removeFromQueue}
+          />
         </div>
         <Button
           text="Очистить"
-          />
+          onClick={clearQueue}
+        />
       </div>
       <div className={styles.circle__container}>
-          <Circle/>
-          <Circle/>
-          <Circle/>
-          <Circle/>
-          <Circle/>
-          <Circle/>
-          <Circle/>
+        {stackQueue.map((char, index) => {
+          return <Circle
+            key={index}
+            letter={char}
+            index={index}/>
+        })}
       </div>
     </SolutionLayout>
   );
