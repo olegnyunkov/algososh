367eba40b43734c1dbd6943c8e70ed7ce04fff59
Нюньков Олег Владимирович <Nyunkov.O@dns-shop.ru>
2022-10-25 17:41:41 +0600

    dev: list in process

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, {useEffect, useState} from "react";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
 import {Input} from "../ui/input/input";
@@ -6,6 +7,7 @@ import styles from "./list-page.module.css"
 import {Circle} from "../ui/circle/circle";
 import {ArrowIcon} from "../ui/icons/arrow-icon";
 import {ElementStates} from "../../types/element-states";
+import { nanoid } from "nanoid";
 
 export type TArrElement = {
   letter: number | null;
@@ -19,20 +21,33 @@ export type TArrElement = {
 
 export const ListPage: React.FC = () => {
   const arrElement: TArrElement = {
-    letter: Math.floor(Math.random()*100),
+    letter: null,
     state: ElementStates.Default,
     head: "",
     tail: "",
     isSmall: false,
     id: null,
   }
-  // const startArray: number[] = [0, 34, 8, 1]
-  const array: TArrElement[] = Array.from({length: 4}, () => arrElement)
-
+  
+  const generateRandomArray = () => {
+    const array = [];
+    for (let i = 0; i <= 3; i++) {
+      const arrayNumbers = Math.floor(Math.random() * 100);
+      array.push({
+        letter: arrayNumbers,
+        state: ElementStates.Default,
+        head: "",
+        tail: "",
+        isSmall: false,
+        id: null,
+      })
+    }
+    return array
+  }
 
   const [inputValue, setInputValue] = useState<number>()
   const [inputIndex, setInputIndex] = useState<number>()
-  const [listArray, setListArray] = useState<TArrElement[]>([...array])
+  const [listArray, setListArray] = useState<TArrElement[]>([...generateRandomArray()])
 
   const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(+e.target.value)
@@ -50,43 +65,102 @@ export const ListPage: React.FC = () => {
     })
   }
 
-  const addToHead = () => {
-    //@ts-ignore
-    arrElement.letter = inputValue
-    //@ts-ignore
-    arrElement.head = <Circle
-      letter={inputValue}
-      state={ElementStates.Changing}
-      isSmall={true}/>
-    arrElement.state = ElementStates.Modified
+  const addToHead = async () => {
+    listArray[0].head = <Circle
+    letter={inputValue}
+    state={ElementStates.Changing}
+    isSmall={true}/>
+    setListArray([...listArray]);
+    await timer()
+    arrElement.letter = inputValue        
     inputValue && listArray.unshift(arrElement);
+    listArray[0].state = ElementStates.Modified
+    listArray[0].head = "head"
+    listArray[1].head = ""
+    setListArray([...listArray]);
+    await timer()
+    listArray[0].state = ElementStates.Default        
     setListArray([...listArray]);
   }
-  const addToTail = () => {
-    //@ts-ignore
+
+  const addToTail = async () => {
+    listArray[listArray.length - 1].tail = <Circle
+    letter={inputValue}
+    state={ElementStates.Changing}
+    isSmall={true}/>
+    setListArray([...listArray]);
+    await timer()
     arrElement.letter = inputValue
     inputValue && listArray.push(arrElement);
+    listArray[listArray.length - 1].state = ElementStates.Modified
+    listArray[listArray.length - 1].tail = "tail"
+    listArray[listArray.length - 2].tail = ""
+    setListArray([...listArray]);
+    await timer()
+    listArray[listArray.length - 1].state = ElementStates.Default        
     setListArray([...listArray]);
   }
-  const removeFromHead = () => {
-    inputValue && listArray.shift();
+
+  const removeFromHead = async () => {
+    listArray[0].head = <Circle
+    letter={listArray[0].letter}
+    state={ElementStates.Changing}
+    isSmall={true}/>
+    listArray[0].letter = null
+    setListArray([...listArray]);
+    await timer()
+    listArray.shift();
     setListArray([...listArray]);
   }
-  const removeFromTail = () => {
-    inputValue && listArray.pop();
+
+  const removeFromTail = async () => {
+    listArray[listArray.length - 1].tail = <Circle
+    letter={listArray[listArray.length - 1].letter}
+    state={ElementStates.Changing}
+    isSmall={true}/>
+    listArray[listArray.length - 1].letter = null
+    setListArray([...listArray]);
+    await timer()
+    listArray.pop();
     setListArray([...listArray]);
   }
-  const addByIndex = () => {
-    //@ts-ignore
+
+  const addByIndex = async () => {
+    
+    listArray[inputIndex].head = <Circle
+    letter={inputValue}
+    state={ElementStates.Changing}
+    isSmall={true}/>
+    setListArray([...listArray]);
+    await timer()
     arrElement.letter = inputValue
     inputIndex && listArray.splice(inputIndex, 0, arrElement);
+    listArray[inputIndex + 1].head = ""
+    listArray[inputIndex].state = ElementStates.Modified
+    setListArray([...listArray]);
+    await timer()
+    listArray[inputIndex].state = ElementStates.Default
     setListArray([...listArray]);
   }
-  const removeByIndex = () => {
-    inputIndex && listArray.splice(inputIndex, 1);
+
+  const removeByIndex = async () => {
+    listArray[inputIndex].tail = <Circle
+    letter={listArray[inputIndex].letter}
+    state={ElementStates.Changing}
+    isSmall={true}/>
+    setListArray([...listArray]);
+    listArray[inputIndex].letter = null
+    await timer()    
+    listArray.splice(inputIndex, 1);
     setListArray([...listArray]);
   }
-  console.log(array)
+
+  useEffect(() => {
+    generateRandomArray()
+  }, [])
+  
+  console.log(Math.floor(Math.random() * 100));
+  
   return (
     <SolutionLayout title="Связный список">
       <div className={styles.list__controls}>
@@ -129,7 +203,8 @@ export const ListPage: React.FC = () => {
                 head={item.head}
                 tail={item.tail}
                 isSmall={item.isSmall}/>
-              <ArrowIcon/>
+              <ArrowIcon
+              key={nanoid()}/>
             </>
           )
         })}
