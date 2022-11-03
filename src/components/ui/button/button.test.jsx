90abac6ec5b19dc99b110c4984ecfe98714c03f0
Нюньков Олег Владимирович <Nyunkov.O@dns-shop.ru>
2022-11-03 19:01:43 +0600

    dev: button test

--- src/components/ui/button/button.test.jsx
+++ src/components/ui/button/button.test.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import renderer from "react-test-renderer";
 import {Button} from "./button";
 
-it("button rendered", () => {
-  const btn = renderer.create(<Button text={""}/>).toJSON();
-  expect(btn).toMatchSnapshot();
-});
+describe("button rendered: ", () => {
+
+  it("without text", () => {
+    const btn = renderer.create(<Button />).toJSON();
+    expect(btn).toMatchSnapshot();
+  });
+
+  it("with text", () => {
+    const btn = renderer.create(<Button text={"Кнопка"}/>).toJSON();
+    expect(btn).toMatchSnapshot();
+  });
+
+  it("with disabled state", () => {
+    const btn = renderer.create(<Button disabled={true}/>).toJSON();
+    expect(btn).toMatchSnapshot();
+  });
+
+  it("with loading state", () => {
+    const btn = renderer.create(<Button isLoader={true}/>).toJSON();
+    expect(btn).toMatchSnapshot();
+  });
+})
